a019a1068e9cd99d261f6c500d945b33f9f9979f
adwaith <adwaithmanojkumar@gmail.com>
2024-06-20 20:15:30 +0530

    Second commit

---
+++ src/main/java/com/springprojects/virtualbookstore/book/BookRepo.java
@@ -0,0 +1,7 @@
+package com.springprojects.virtualbookstore.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepo extends JpaRepository<Book, Integer>{
+
+}
