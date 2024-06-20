fa8f27496fd8c948b7f06c38a9b34150fa35c8a5
adwaith <adwaithmanojkumar@gmail.com>
2024-06-20 20:03:11 +0530

    Initial Commit

---
+++ src/test/java/com/springprojects/virtualbookstore/VirtualbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojects.virtualbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
